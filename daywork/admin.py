1e53b48f9b9b8ff2ef0b2d231da320b5a2295031
hongmingu <gmh397@nate.com>
2018-05-10 16:13:07 +0900

    before deploy

--- daywork/admin.py
+++ daywork/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from daywork.models import *
 
 admin.site.register(DayWork)
-admin.site.register(DayWorkArabic)
-admin.site.register(DayWorkChinese)
-admin.site.register(DayWorkEnglish)
-admin.site.register(DayWorkPortuguese)
-admin.site.register(DayWorkSpanish)
 
 
 # Register your models here.
