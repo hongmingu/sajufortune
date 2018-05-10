59203d5ab2e2ed608cc78697a868303b6ec53c5c
hongmingu <gmh397@nate.com>
2018-05-10 15:54:18 +0900

    before deploy

--- dayemotion/admin.py
+++ dayemotion/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from dayemotion.models import *
 
 admin.site.register(DayEmotion)
-admin.site.register(DayEmotionArabic)
-admin.site.register(DayEmotionChinese)
-admin.site.register(DayEmotionEnglish)
-admin.site.register(DayEmotionPortuguese)
-admin.site.register(DayEmotionSpanish)
 
 
 # Register your models here.
--- daymoney/admin.py
+++ daymoney/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from daymoney.models import *
 
 admin.site.register(DayMoney)
-admin.site.register(DayMoneyArabic)
-admin.site.register(DayMoneyChinese)
-admin.site.register(DayMoneyEnglish)
-admin.site.register(DayMoneyPortuguese)
-admin.site.register(DayMoneySpanish)
 
 
 # Register your models here.
